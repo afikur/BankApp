f10995de5118eaff4c92c467dbff14aa2f652ca5
Afikur Rahman <this.afikur@gmail.com>
2018-04-03 05:05:10 +0600

    add controllers and model

--- src/main/java/com/dsi/spring/controllers/AccountController.java
+++ src/main/java/com/dsi/spring/controllers/AccountController.java
@@ -1,12 +1,46 @@
 package com.dsi.spring.controllers;
 
+import com.dsi.spring.model.Account;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AccountController {
     @RequestMapping("/")
     public String showHomePage() {
         return "index";
+    }
+
+    @RequestMapping("/new")
+    public String newAccount() {
+        return "newAccount";
+    }
+
+    @RequestMapping("/showAccount")
+    public String showAccount() {
+        return "showAccount";
+    }
+
+    @RequestMapping(value = "/saveAccount", method = RequestMethod.POST)
+    public String saveAccount(Model model, HttpServletRequest request) {
+        String acNo = request.getParameter("accountNo");
+        String customerName = request.getParameter("accountHolderName");
+        String balance = request.getParameter("balance");
+
+//        model.addAttribute("accountNumber", acNo);
+//        model.addAttribute("accountHolderName", customerName);
+//        model.addAttribute("balance", balance);
+
+        Account account = new Account();
+        account.setAccountNo(Integer.parseInt(acNo));
+        account.setAccountHolderName(customerName);
+        account.setBalance(Integer.parseInt(balance));
+
+        model.addAttribute("account", account);
+        return "showAccount";
     }
 }
